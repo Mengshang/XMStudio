6bc12727c7a9dda68db93d709bdebe5d83e430cf
HanYiXin <a3475266020@163.com>
2022-07-14 11:45:11 +0800

    no msg

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/core/CoreHeader.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "CoreHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
